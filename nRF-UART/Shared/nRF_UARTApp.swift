6cb4976f926a4db246e0b16d95886c2d75839cc9
Nick Kibysh <nick.kibysh@nordicsemi.no>
2021-11-16 20:00:42 +0100

    created project

---
+++ nRF-UART/Shared/nRF_UARTApp.swift
@@ -0,0 +1,20 @@
+//
+//  nRF_UARTApp.swift
+//  Shared
+//
+//  Created by Nick Kibysh on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct nRF_UARTApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
