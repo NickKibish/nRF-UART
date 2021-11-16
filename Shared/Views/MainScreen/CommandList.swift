6c90b851f7132095e083d4cfc769e5c8b8ea39ae
Nick Kibysh <nick.kibysh@nordicsemi.no>
2021-11-16 22:20:27 +0100

    added color asset and color manager. Created 'add command' view

---
+++ Shared/Views/MainScreen/CommandList.swift
@@ -0,0 +1,20 @@
+//
+//  CommandList.swift
+//  nRF-UART
+//
+//  Created by Nick Kibysh on 16/11/2021.
+//
+
+import SwiftUI
+
+struct CommandList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommandList_Previews: PreviewProvider {
+    static var previews: some View {
+        CommandList()
+    }
+}
