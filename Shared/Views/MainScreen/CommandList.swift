279a9dc55aaca01acca6ee9ce887977581bc017d
Nick Kibysh <nick.kibysh@nordicsemi.no>
2021-11-23 17:57:35 +0100

    - set command list as root view - added blank 'createNewCommand' view

--- Shared/Views/MainScreen/CommandList.swift
+++ Shared/Views/MainScreen/CommandList.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct CommandList: View {
+    @State var newCommandPresented = false
+    
+    let data = (1...1).map { "Item \($0)" }
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 80))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        #if os(OSX)
+//        contentView()
+        VStack {
+            Text("Hello World")
+        }
+        #else
+        NavigationView {
+            contentView()
+        }
+        .navigationViewStyle(.stack)
+        #endif
+    }
+    
+    @ViewBuilder
+    func contentView() -> some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(data, id: \.self) { item in
+                    Button(action: {
+                        newCommandPresented.toggle()
+                    }, label: {
+                        AddCommand()
+                            .frame(height: 100)
+                    })
+                        .sheet(isPresented: $newCommandPresented, content: {
+                            CreateNewCommand()
+                        })
+                }
+            }
+            .padding(.horizontal)
+            .navigationTitle("Commands")
+        }
     }
 }
 
 struct CommandList_Previews: PreviewProvider {
     static var previews: some View {
-        CommandList()
+        Group {
+            CommandList()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro"))
+            
+//            CommandList()
+//                .previewDevice(PreviewDevice("iPad Air (4th generation)"))
+            
+        }
     }
 }
--- Shared/nRF_UARTApp.swift
+++ Shared/nRF_UARTApp.swift
@@ -13,7 +13,7 @@ struct nRF_UARTApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CommandList()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
